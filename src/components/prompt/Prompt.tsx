7a513f8516c4001a3e60b3a2b70edaf5d9689693
오승태 <yeoularu@gmail.com>
2024-10-07 02:42:11 +0900

    feat: collapse all btn first, then show drag grip

--- src/components/prompt/Prompt.tsx
+++ src/components/prompt/Prompt.tsx
@@ -6,7 +6,10 @@ import {
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
 import { cn } from '@/lib/utils';
-import { $currentPromptSet } from '@/stores/currentPromptSet';
+import {
+  $currentPromptsCreatedAt,
+  $currentPromptSet,
+} from '@/stores/currentPromptSet';
 import { useStore } from '@nanostores/react';
 import { debounce } from 'es-toolkit';
 import {
@@ -16,7 +19,7 @@ import {
   ChevronsDownUpIcon,
   ChevronsUpDownIcon,
 } from 'lucide-react';
-import { useMemo, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import TextareaAutosize from 'react-textarea-autosize';
 import { useSortable } from '@dnd-kit/sortable';
 import { CSS } from '@dnd-kit/utilities';
@@ -79,27 +82,53 @@ export default function Prompt({ createdAt }: Readonly<{ createdAt: number }>) {
 
   const getCurrentPrompts = () => $currentPromptSet.get().prompts;
 
+  const [isDragPossible, setIsDragPossible] = useState(false);
+  useEffect(() => {
+    if (!isFocused) return;
+    setIsDragPossible(
+      collapsedPromptsCreatedAt.length + 1 ===
+        $currentPromptsCreatedAt.get().length
+    );
+  }, [collapsedPromptsCreatedAt.length, isFocused]);
+
+  const handleBeforeDragStart = () => {
+    collapseAllPrompts();
+  };
+
   return (
     <div
       ref={setNodeRef}
       style={style}
       className={cn('relative flex', isDragging && 'z-50 opacity-50')}
     >
-      <Button
-        variant="outline"
-        className={cn(
-          'absolute inset-x-0 -top-3 z-50 mx-auto hidden h-2 w-fit cursor-grab touch-none',
-          (isFocused || isDragging) && 'flex',
-          isDragging && 'cursor-grabbing'
-        )}
-        onMouseDown={collapseAllPrompts}
-        onTouchStartCapture={collapseAllPrompts}
-        {...attributes}
-        {...listeners}
-      >
-        <GripHorizontalIcon className="h-4 w-4" />
-      </Button>
-
+      {isDragPossible ? (
+        <Button
+          variant="outline"
+          className={cn(
+            'absolute inset-x-0 -top-3 z-50 mx-auto hidden h-2 w-fit cursor-grab touch-none',
+            (isFocused || isDragging) && 'flex',
+            isDragging && 'cursor-grabbing'
+          )}
+          onMouseDown={handleBeforeDragStart}
+          onTouchStartCapture={handleBeforeDragStart}
+          {...attributes}
+          {...listeners}
+        >
+          <GripHorizontalIcon className="h-4 w-4 text-accent-foreground" />
+        </Button>
+      ) : (
+        <Button
+          variant="outline"
+          className={cn(
+            'absolute inset-x-0 -top-3 z-50 mx-auto hidden h-2 w-fit',
+            isFocused && 'flex'
+          )}
+          onMouseDown={handleBeforeDragStart}
+          onTouchStartCapture={handleBeforeDragStart}
+        >
+          <ChevronsDownUpIcon className="h-3 w-3" />
+        </Button>
+      )}
       <div>
         <Button
           variant="ghost"
@@ -121,7 +150,6 @@ export default function Prompt({ createdAt }: Readonly<{ createdAt: number }>) {
           </div>
         </Button>
       </div>
-
       <TextareaAutosize
         id={createdAt.toString()}
         defaultValue={
@@ -133,7 +161,10 @@ export default function Prompt({ createdAt }: Readonly<{ createdAt: number }>) {
         }}
         onChange={handleTextareaChange}
         onFocus={() => setIsFocused(true)}
-        onBlur={() => setIsFocused(false)}
+        onBlur={() => {
+          !isOverflown && setCollapsed(true);
+          setIsFocused(false);
+        }}
         onHeightChange={(height) => setTextareaHeight(height)}
         placeholder={
           getCurrentPrompts().findIndex(({ createdAt: c }) => c === createdAt)
@@ -148,7 +179,6 @@ export default function Prompt({ createdAt }: Readonly<{ createdAt: number }>) {
           collapsed && 'max-h-16'
         )}
       />
-
       <DropdownMenu open={open} onOpenChange={(o) => setOpen(o)}>
         <DropdownMenuTrigger asChild>
           <Button variant="ghost" className="sticky top-16 h-16 w-5 p-0.5">
@@ -163,14 +193,12 @@ export default function Prompt({ createdAt }: Readonly<{ createdAt: number }>) {
           <RemovePromptBtn createdAt={createdAt} />
         </DropdownMenuContent>
       </DropdownMenu>
-
       <div
         className={cn(
           'absolute inset-y-0 w-px bg-primary peer-focus-visible:bg-muted',
           open && 'bg-muted'
         )}
       />
-
       {collapsed && isOverflown && (
         <div className="pointer-events-none absolute inset-x-0 -bottom-1 flex h-8 items-end bg-gradient-to-b from-transparent to-background">
           <ChevronDownIcon className="mx-auto mb-1 mt-auto h-4 w-4 rounded-full bg-background text-primary dark:bg-transparent" />
